7b46d7ee20de80542c9e11688eb45f675243f186
aulyanov <aulyanov@10.25.144.38>
2016-09-09 17:48:41 -0400

    Added JOption input.

--- Third/My/myclasses/TextToASCII.java
+++ Third/My/myclasses/TextToASCII.java
@@ -1,8 +1,11 @@
 package myclasses;
+import javax.swing.JOptionPane;
 
 public class TextToASCII {
 	
-	private static final String TEXT = "Software Engineering";
+	//private static final String TEXT = "Software Engineering";
+	
+	private static final String TEXT = JOptionPane.showInputDialog("Please input a text:");
 
 	public static void main(String[] args) {
 		String asciiResult = String.format("ASCII result of [%s] : ", TEXT);
