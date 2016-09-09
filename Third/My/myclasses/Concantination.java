737761353515d25595ff0123e71802f1ad80cdb5
Bohdan <bogdan.sharipov@yandex.ru>
2016-09-09 13:42:30 -0400

    Update Concantination.java

--- Third/My/myclasses/Concantination.java
+++ Third/My/myclasses/Concantination.java
@@ -3,6 +3,7 @@ package myclasses;
 public class Concantination {
 
 	public static void main(String[] args) {
+		
 		System.out.println(" " + 10 + 20);
 		System.out.println(" " + (10 + 20));
 		System.out.println(10 + 20 + " ");
