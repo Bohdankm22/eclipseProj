e236e890f082ba7621636fa65aa531a635a68c02
bohdansharipov <bohdansharipov@10.24.50.81>
2016-09-09 13:48:18 -0400

    Adding couple methods to concant class

--- Third/My/myclasses/Concantination.java
+++ Third/My/myclasses/Concantination.java
@@ -3,13 +3,15 @@ package myclasses;
 public class Concantination {
 
 	public static void main(String[] args) {
-		
-		System.out.println(" " + 10 + 20);
-		System.out.println(" " + (10 + 20));
-		System.out.println(10 + 20 + " ");
-		System.out.println("The sum of " + 10 + " and " + 20 + " = " + (10 + 20));
-		
-		
+		print(operation("The sum of " + 10 + " and " + 20 + " = " + (10 + 20)));
+	}
+	
+	public static void print(String text) {
+		System.out.println(text);
+	}
+	
+	public static String operation(String arg) {
+		return "Answer is " + arg;
 	}
 
 }
