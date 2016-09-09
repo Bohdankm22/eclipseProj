93d3188edc464ac53be2ce977be9925d539fc1bc
bohdansharipov <bohdansharipov@10.24.50.81>
2016-09-09 13:38:30 -0400

    Third commit

---
+++ Third/My/myclasses/Concantination.java
@@ -0,0 +1,14 @@
+package myclasses;
+
+public class Concantination {
+
+	public static void main(String[] args) {
+		System.out.println(" " + 10 + 20);
+		System.out.println(" " + (10 + 20));
+		System.out.println(10 + 20 + " ");
+		System.out.println("The sum of " + 10 + " and " + 20 + " = " + (10 + 20));
+		
+		
+	}
+
+}
