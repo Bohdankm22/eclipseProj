f46e739e4399efc8d0f2785df648c4107db9d532
bohdansharipov <bohdansharipov@10.24.50.81>
2016-09-09 13:00:44 -0400

    First commit

---
+++ Third/My/myclasses/Main.java
@@ -0,0 +1,13 @@
+package myclasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("7 + 9 = " + (7 + 9));
+		System.out.println("45 - 12 = " + (45 - 12));
+		System.out.println("11 * 4 = " + 11 * 4);
+		System.out.println("23 / 5 = " + 23 / 5);
+		System.out.println("23 % 5 = " + 23 % 5);
+	}
+
+}
