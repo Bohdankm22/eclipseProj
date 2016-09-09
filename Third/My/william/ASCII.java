29851d1014f37b9b2da5b97f6b538b592cb60b59
Guillermo <Guillermo@10.24.13.212>
2016-09-09 15:54:34 -0400

    update

--- Third/My/william/ASCII.java
+++ Third/My/william/ASCII.java
@@ -1,5 +1,27 @@
 package william;
 
 public class ASCII {
-
+	public static void main (String[] args) {
+		System.out.println("(int)('S') = "+(int)('S'));
+		System.out.println("(int)('o') = "+(int)('o'));
+		System.out.println("(int)('f') = "+(int)('f'));
+		System.out.println("(int)('t') = "+(int)('t'));
+		System.out.println("(int)('w') = "+(int)('w'));
+		System.out.println("(int)('a') = "+(int)('a'));
+		System.out.println("(int)('r') = "+(int)('r'));
+		System.out.println("(int)('e') = "+(int)('e'));
+		System.out.println("(int)(' ') = "+(int)(' '));
+		System.out.println("(int)('E') = "+(int)('E'));
+		System.out.println("(int)('n') = "+(int)('n'));
+		System.out.println("(int)('g') = "+(int)('g'));
+		System.out.println("(int)('i') = "+(int)('i'));
+		System.out.println("(int)('n') = "+(int)('n'));
+		System.out.println("(int)('e') = "+(int)('e'));
+		System.out.println("(int)('e') = "+(int)('e'));
+		System.out.println("(int)('r') = "+(int)('r'));
+		System.out.println("(int)('i') = "+(int)('i'));
+		System.out.println("(int)('n') = "+(int)('n'));
+		System.out.println("(int)('g') = "+(int)('g'));
+	
+	}
 }
