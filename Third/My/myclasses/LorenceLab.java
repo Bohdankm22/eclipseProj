2882d1b22a53882eef4fa0ed586a136f3dba1e50
bohdansharipov <bohdansharipov@10.24.50.81>
2016-09-09 16:17:01 -0400

    Adding weman formula

--- Third/My/myclasses/LorenceLab.java
+++ Third/My/myclasses/LorenceLab.java
@@ -3,13 +3,15 @@ package myclasses;
 public class LorenceLab {
 
 	public static void main(String[] args) {
-		int height, weight;
+		int height, weight, weight1;
 		String firstName, lastName;
 		height = 182;
 		firstName = "Bohdan";
 		lastName = "Sharipov";
-		weight = (int) ((height - 100) - (height - 150) / 4);;
-		System.out.println("The ideal weight for you is " + weight);
+		weight = (int) ((height - 100) - (height - 150) / 4);
+		weight1 = (int) ((height - 100) - (height - 150) / 2.5);
+		System.out.println("The ideal weight for man is " + weight);
+		System.out.println("The ideal weight for weman is " + weight1);
 	}
 
 }
