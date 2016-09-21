494ac6dee8cdc9fc227a642841e1620903c1f403
Guillermo <Guillermo@10.24.13.69>
2016-09-21 18:54:21 -0400

    others

--- Third/My/william/IdealWeight.java
+++ Third/My/william/IdealWeight.java
@@ -1,5 +1,19 @@
 package william;
 
-public class IdealWeight {
+import javax.swing.JOptionPane;
 
+public class IdealWeight {
+	public static void main (String[] args) {
+		double h, w;
+		String fN, lN, m;
+		fN = JOptionPane.showInputDialog("Provide your first name and press OK");
+		lN = JOptionPane.showInputDialog("Provide your last name and press OK");
+		h = Double.parseDouble(JOptionPane.showInputDialog("Provide your height and press OK"));
+		w = ((h*100)-100)-((h*100)-150)/4;
+		m = String.format("WEIGHT REPORT MONITORING")+String.format("---------------------")+
+				String.format(" ")+String.format("First Name: "+fN)+String.format("Last Name: "+lN)+
+				String.format("Height: "+h)+String.format("Ideal Weight: "+w);
+		JOptionPane.showMessageDialog(null, m,"Ideal Weight",JOptionPane.INFORMATION_MESSAGE);
+		
+	}
 }
