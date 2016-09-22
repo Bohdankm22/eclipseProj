e2ad67291d49bfcd83d2b4175a77eaa575d2b460
dtarasyuk <dtarasyuk@hortonworks.com>
2016-09-22 18:17:02 -0400

    Lab14 - Factorial in infinit loop

---
+++ Third/My/oopclasses/FactorialLab14.java
@@ -0,0 +1,27 @@
+package oopclasses;
+
+import javax.swing.JOptionPane;
+
+public class FactorialLab14 {
+
+	public static void main(String[] args) {
+		boolean exit = false;
+		while (!exit) {
+			int number = Integer.parseInt(JOptionPane.showInputDialog("Please input number: "));
+			JOptionPane.showMessageDialog(null, String.format("Factorial of %d is %d", number, calcFact(number)), " ", JOptionPane.INFORMATION_MESSAGE);
+			String answer = JOptionPane.showInputDialog("Do you want to continue? (Y/y) or (N/n)");
+			if (answer.toLowerCase().equals("y")) {
+				continue;
+			} else if(answer.toLowerCase().equals("n")) {
+				System.exit(0);
+			} else {
+				JOptionPane.showMessageDialog(null, "Invalid input", " ", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+	
+	private static long calcFact(int numb) {
+		return numb == 1 ? 1 : numb * calcFact(numb -1);
+	}
+
+}
