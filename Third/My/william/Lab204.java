06d3c762c7ca67005e683fa6d26231adf994d747
Guillermo <Guillermo@10.24.13.84>
2016-09-28 18:07:35 -0400

    Labs

--- Third/My/william/Lab204.java
+++ Third/My/william/Lab204.java
@@ -1,5 +1,22 @@
 package william;
 
-public class Lab204 {
+import javax.swing.JOptionPane;
 
+public class Lab204 {
+	public static void main(String [] args){
+		int x;
+		x = Integer.parseInt(JOptionPane.showInputDialog("Provide a number with two digits"));
+		int r = reverse(x);
+		System.out.println("the number is "+r);
+	}
+	public static int reverse(int x){
+		int y, r=0;
+		while(x != 0)
+		  {
+			y = x % 10;
+            x = x / 10;
+            r = r * 10 + y;
+		  }
+		return r;
+	}
 }
--- Third/My/william/Lab205.java
+++ Third/My/william/Lab205.java
@@ -1,5 +1,30 @@
 package william;
 
-public class Lab205 {
+import javax.swing.JOptionPane;
 
+public class Lab205 {
+	public static void main(String [] args){
+		int a,b,c;
+		a = Integer.parseInt(JOptionPane.showInputDialog("Provide 'a'"));
+		b = Integer.parseInt(JOptionPane.showInputDialog("Provide 'b'"));
+		c = Integer.parseInt(JOptionPane.showInputDialog("Provide 'c'"));
+		double x1 = calQ1(a,b,c);
+		double x2 = calQ2(a,b,c);
+		System.out.println("The results are "+x1+" and "+x2);
+	}
+	public static int calD(int a, int b, int c){
+		int d;
+		d=(b*b)-(4*a*c);
+		return d;
+	}
+	public static double calQ1(int d, int b, int a){
+		double x1;
+		x1=((-b+(Math.sqrt(d)))/2*a);
+		return x1;
+	}
+	public static double calQ2(int d, int b, int a){
+		double x2;
+		x2=((-b-(Math.sqrt(d)))/2*a);
+		return x2;
+	}
 }
