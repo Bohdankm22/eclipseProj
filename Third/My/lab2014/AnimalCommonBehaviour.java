f35fa13b3d2f6930805c8544a2fdbaa1975ed2f8
Guillermo <Guillermo@10.24.13.166>
2016-11-01 13:14:11 -0400

    simple polymorphism lab

---
+++ Third/My/lab2014/AnimalCommonBehaviour.java
@@ -0,0 +1,13 @@
+package lab2014;
+
+public abstract class AnimalCommonBehaviour implements Animals {
+	public void sleep(){
+		System.out.println("Eyes closed");
+	}
+	public void breath(){
+		System.out.println("Inhaling and exhaling");
+	}
+	public abstract void talk();
+	public abstract void standing();
+	public abstract void craft();
+}
---
+++ Third/My/lab2014/Animals.java
@@ -0,0 +1,11 @@
+package lab2014;
+
+public interface Animals {
+	public void sleep();
+	public void breath();
+	public void talk();
+	public void standing();
+	public void craft();
+	public void fight();
+	public void balancing();
+}
