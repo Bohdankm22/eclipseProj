be5deb4f31c2dd48758c0e197cfeffbfc377b0d2
aulyanov <aulyanov@172.16.181.63>
2016-11-02 15:07:06 -0400

    descendig double array

--- Third/My/MidTerm/Huffman.java
+++ Third/My/MidTerm/Huffman.java
@@ -2,6 +2,8 @@ package MidTerm;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Huffman {
 
@@ -80,10 +82,18 @@ public class Huffman {
 		//5. Ordered_Frequency array Start
 		
 		Arrays.sort(Frequency);
+		
 		System.out.println(Arrays.toString(Frequency)); //for test
 		//finished on this part
+		//test array
+//		double[] d2 = new double[] { 5, 2, 3, 1, 4 };
+//	    Arrays.sort(d2, d2.length, 1);
+//	    for (double d: d2){
+//		      System.out.print(" " +d);
+//		      {
 		
 		//5. End
+	  
 	}
 
 }
