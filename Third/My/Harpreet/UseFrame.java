f0bb80bad1ce0bf0d88eb5ab4bec8cfb74b6d6b6
Guillermo <Guillermo@10.24.15.8>
2016-11-04 12:52:19 -0400

    Random

---
+++ Third/My/Harpreet/UseFrame.java
@@ -0,0 +1,11 @@
+package Harpreet.assignment3;
+
+import java.awt.Toolkit;
+
+public class UseFrame { 
+	public static void main(String[] args) {
+		FrameClass frame = new FrameClass();
+		
+	}
+
+}
