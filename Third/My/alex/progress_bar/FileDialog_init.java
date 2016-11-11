90d75da3eee7e0e5926fad447d275cb969d1b209
aulyanov <aulyanov@172.16.181.63>
2016-11-11 10:56:07 -0500

    Toolbar with File Dialog class lab

---
+++ Third/My/alex/progress_bar/FileDialog_init.java
@@ -0,0 +1,22 @@
+package alex.progress_bar;
+
+import java.awt.FileDialog;
+
+import javax.swing.JFrame;
+
+public class FileDialog_init {
+
+	public static void main(String[] args) {
+		
+		//int rep = JOptionPane.showConfirmDialog(null, "Exit now?");
+		//String rep = JOptionPane.showInputDialog(null, "Exit now?");
+		//JOptionPane.showMessageDialog(null, "XYU");
+		
+		ToolBarwFD f = new ToolBarwFD();
+		f.pack();
+		f.setVisible(true);
+
+		
+	}
+	
+}
