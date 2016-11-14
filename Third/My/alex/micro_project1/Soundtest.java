0ad229b6e0532e288b7ded6be0a4a63ccea4429e
Bohdan <bogdan.sharipov@yandex.ru>
2016-11-13 21:51:12 -0500

    Proj stage 1

--- Third/My/alex/micro_project1/Soundtest.java
+++ Third/My/alex/micro_project1/Soundtest.java
@@ -4,114 +4,75 @@ package alex.micro_project1;
 import java.awt.FileDialog;
 import java.awt.FlowLayout;
 import java.io.File;
-import java.io.FileInputStream;
-
-
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
 
-import sun.audio.*;
-
-
-//test1
-
 public class Soundtest extends JFrame {
 	
-	static Clip clip;
-	
-	static JProgressBar current;
-	static JTextArea out;
-	static JButton find;
-	Thread runner;
-
-	public static void main(String[] args) {
-			
-		
-		
-		
-		//System.out.println(Clap.getAbsolutePath());
-		//System.out.println(Clap.exists());
-		
-		Soundtest sound = new Soundtest();	
-		
-		
-	}
+	Clip clip;
+	JProgressBar progressBar;
+	JTextArea textArea;
+//	JButton button;
+	File soundFile;
 	
 	public Soundtest(){
 		
 		setTitle("Progress");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JFrame b = new JFrame();
+		FileDialog fd = new FileDialog(b, "Pick a file:", FileDialog.LOAD);
+		fd.setVisible(true);
+		
+		soundFile = new File(fd.getDirectory() + fd.getFile());
+	}
+	
+	private void showProgressBar(long micro) {
 		JPanel p = new JPanel();
 		p.setLayout(new FlowLayout());
-		current = new JProgressBar(0, 100); //
-		current.setValue(0);
-		current.setStringPainted(true);
-		p.add(current);
+		progressBar = new JProgressBar(0, (int)micro / 1000); //
+		progressBar.setValue(0);
+		progressBar.setStringPainted(true);
+		textArea = new JTextArea();
+		textArea.setText("Duration is " + micro + " microseconds \nCurrent fragment is 0");
+		p.add(textArea);
+		p.add(progressBar);
 		setContentPane(p);
 		pack();
 		setVisible(true);
-			
-		try {
-			clip = AudioSystem.getClip();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-		} 
-		
-		JFrame b = new JFrame();
-		FileDialog fd = new FileDialog(b, "Pick a file:", FileDialog.LOAD);
-		//fd.setFile(".pdf");
-		fd.setVisible(true);
-		
-		String filename = fd.getFile();
-		System.out.println(filename);
-		String a = fd.getDirectory();
-		System.out.println(a+filename);
-		
-		//File Clap = new File("sound_converted.wav");
-		
-		File Clap = new File(a+filename);
-		
-		
-		playMedia(Clap);
-		
-		
-		iterate(clip.getMicrosecondLength());		
-		
+	}
+	
+	public void startPlaying() {
+		playMedia(soundFile);
+		showProgressBar(clip.getMicrosecondLength());
+		iterate(clip.getMicrosecondLength());
 	}
 
-	public void iterate(long durInMicro){
+	private void iterate(long durInMicro){
 		int num = 0;
 		int durInMili = Math.round(durInMicro / 1000);
 		while (num <= durInMili){
-			current.setValue(num);
+			progressBar.setValue(num);
+			textArea.setText("Duration is " + durInMicro + " microseconds \nCurrent fragment is " + num * 1000);
 			try{
-				Thread.sleep(1000);
-				
+				Thread.sleep(1);
 			} catch (InterruptedException e) {}
-			num += 1000;
+			num += 1;
 		}
 		
 	}
-	public void playMedia(File Sound) {
-		
-		
+	
+	private void playMedia(File Sound) {
         try {
+        	clip = AudioSystem.getClip();
         	clip.open(AudioSystem.getAudioInputStream(Sound));
         	clip.start();
-
-        	//Thread.sleep(5000);
-        	
-//        	Thread.sleep(clip.getMicrosecondLength()/1000);
-        	
-        } catch(Exception e) {
-        	
+        } catch(Exception e) {	
+        	System.out.println(e);
       }
 	}
 }
